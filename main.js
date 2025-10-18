2add9d1cabdc0a5d28263d30f3502fcf8a614c38
pbain63 <prodipbain63@gmail.com>
2025-10-18 21:39:53 +0600

    Set isOpen to false because when the page first load, the navigation menu is closed in main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navList = document.querySelector(".nav__list");
+// It's set to false initially because when the page first loads, the navigation menu is closed.
+let isOpen = false;
+
+
 //  if (isOpen) {
 //   navToggle.textContent = "✖"; // Menu is open, show a close icon
 // } else {
 //   navToggle.textContent = "☰"; // Menu is closed, show a hamburger icon
 // }
-
-// It's set to false initially because when the page first loads, the navigation menu is closed.
