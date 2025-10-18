d64ed0a3fa78e24c3a322c3e9222e6b084091326
pbain63 <prodipbain63@gmail.com>
2025-10-18 21:42:09 +0600

    Set event listener to hamburger button and flip the state in main.js

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const navList = document.querySelector(".nav__list");
 // It's set to false initially because when the page first loads, the navigation menu is closed.
 let isOpen = false;
 
-
+navToggle.addEventListener("click", () => {
+  // 1. Flip the state
+  isOpen = !isOpen;
+});
 //  if (isOpen) {
 //   navToggle.textContent = "✖"; // Menu is open, show a close icon
 // } else {
