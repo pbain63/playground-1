71cad3a775e7438197130f25b835eebf15a876b3
pbain63 <prodipbain63@gmail.com>
2025-10-18 21:34:29 +0600

    Select hamburger button and nav list by DOM in main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const navToggle = document.querySelector(".nav-toggle");
+const navList = document.querySelector(".nav__list");
+//  if (isOpen) {
+//   navToggle.textContent = "✖"; // Menu is open, show a close icon
+// } else {
+//   navToggle.textContent = "☰"; // Menu is closed, show a hamburger icon
+// }
+
+// It's set to false initially because when the page first loads, the navigation menu is closed.
