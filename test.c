4b4c26769b3c88eb56904dfd4a057edc6c2abeea
pbain63 <prodipbain63@gmail.com>
2025-11-02 13:28:16 +0600

    Debug test.c,  creates new files and print variable with C language in test.c

---
+++ test.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    ch = 'x';
+    printf("Value stored in ch is %c\n", ch);
+    ch = 'y';
+    printf("Value stored in ch is %c\n", ch);
+    return 0;
+}
