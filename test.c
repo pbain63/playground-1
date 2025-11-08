1087d40e1ba0d4c5b1e11013e624457ea9f8e615
pbain63 <prodipbain63@gmail.com>
2025-11-08 21:39:19 +0600

    Apply if else statement in test.c

--- test.c
+++ test.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 int main()
 {
-    double C, F;
-    scanf("%lf", &F);
-    C = ((F - 32) / 9) * 5;
-    printf("%lf\n", C);
+    int n1 = 5, n2 = 7;
+    if (n1 == n2)
+    {
+        printf("Numbers are equal.");
+    }
+    else
+    {
+        printf("Numbers are not equal.");
+    }
+
     return 0;
 }
