981bcb405a2aed9d79e036e2d5b9d855bc92d669
pbain63 <prodipbain63@gmail.com>
2025-11-17 21:50:14 +0600

    Modify program to print even number another way in test.c

--- test.c
+++ test.c
@@ -2,15 +2,15 @@
 int main()
 {
     int n;
-    n = 1;
+    n = 2;
     while (n <= 100)
     {
-        if (n % 2 == 0)
-        {
-            printf("%d\n", n);
-        }
+        // if (n % 2 == 0)
+        // {
+        printf("%d\n", n);
+        // }
 
-        n = n + 1;
+        n = n + 2;
     }
     return 0;
 }
