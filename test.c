2b0896c700af4b8de169a066de498fb5ccfea2b5
pbain63 <prodipbain63@gmail.com>
2025-11-19 21:33:11 +0600

    Add new program to print odd number from 1 to 100 in test.c

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    n = 1;
+    while (n <= 100)
+    {
+        if (n % 2 == 1)
+        {
+            printf("%d\n", n);
+        }
+        n = n + 1;
+    }
+    return 0;
+}
