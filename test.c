64d2a587da6b34deb638af2fb81070c12ba31784
pbain63 <prodipbain63@gmail.com>
2025-11-21 22:05:20 +0600

    Add new program to print number from 1 to 100 using for loop in test.c

--- test.c
+++ test.c
@@ -2,11 +2,12 @@
 int main()
 {
     int n, sum;
-    sum = 0;
+    // sum = 0;
     for (n = 1; n <= 100; n = n + 1)
     {
-        sum = sum + n;
+        // sum = sum + n;
+         printf("Result: %d\n", n);
     }
-    printf("Result: %d\n", sum);
+   
     return 0;
 }
