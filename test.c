d9bebfd329ef8fbbfcd39362d2637073c88a768c
pbain63 <prodipbain63@gmail.com>
2025-11-25 23:12:57 +0600

    Update multiplication table with user prompt in test.c

--- test.c
+++ test.c
@@ -2,7 +2,9 @@
 int main()
 {
     int i, n;
+    printf("Enter the number for the multiplication table: ");
     scanf("%d", &n);
+    printf("\nMultiplication Table for %d : \n", n);
     for (i = 1; i <= 10; i = i + 1)
     {
         printf("%d x %d = %d\n", n, i, n * i);
