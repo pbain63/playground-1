fb0677e58ea735b11f080676034a98739019df08
pbain63 <prodipbain63@gmail.com>
2025-11-26 22:39:22 +0600

    Add new program to print array in general in test.c

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int marks[5];
+
+    marks[0] = 87;
+    marks[1] = 82;
+    marks[2] = 76;
+    marks[3] = 85;
+    marks[4] = 88;
+
+    printf("%d\n", marks[0]);
+    printf("%d\n", marks[1]);
+    printf("%d\n", marks[2]);
+    printf("%d\n", marks[3]);
+    printf("%d\n", marks[4]);
+    return 0;
+}
