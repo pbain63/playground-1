18a595ffec8b4073c8a5fb5c542dc5ed15226fc5
pbain63 <prodipbain63@gmail.com>
2025-11-30 22:26:54 +0600

    Add new program to take string as input in test.c

--- test.c
+++ test.c
@@ -1,18 +1,8 @@
 #include <stdio.h>
 int main()
 {
-    int ara[] = {10, 20, 30, 40, 50};
-    int n = 5, i;
-    int temp;
-    for (i = 0; i < n / 2; i++)
-    {
-        temp = ara[i];
-        ara[i] = ara[n - 1 - i];
-        ara[n - 1 - i] = temp;
-    }
-    for (i = 0; i < n; i++)
-    {
-        printf("%d\n", ara[i]);
-    }
+    char name[80];
+    scanf("%s", name);
+    printf("%s\n", name);
     return 0;
 }
