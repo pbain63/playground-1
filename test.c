65317135ffc1903852d71f9d93e54e17ef63cffe
pbain63 <prodipbain63@gmail.com>
2025-11-30 22:52:53 +0600

    Add new program to take string as input and calculate the characters of the string in test.c

--- test.c
+++ test.c
@@ -2,7 +2,14 @@
 int main()
 {
     char name[80];
+    int i, length;
     scanf("%s", name);
-    printf("%s\n", name);
+    i = 0;
+    while (name[i] != '\0')
+    {
+        i = i + 1;
+    }
+    length = i;
+    printf("%s has %d characters.\n", name, length);
     return 0;
 }
