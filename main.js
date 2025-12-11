be0afef5bd1ae0c6c1b8d4d0dee03c42408a8d3f
pbain63 <prodipbain63@gmail.com>
2025-12-11 10:34:41 +0600

    Select class of the div's by DOM in main.js

---
+++ main.js
@@ -0,0 +1,3 @@
+const dropdownContainer = document.querySelector(".dropdown-container");
+const menuTitle = document.querySelector(".menu-title");
+const navList = document.querySelector(".nav__list");
