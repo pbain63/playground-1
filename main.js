8898f0accd353daa5a2bed7fec11e0c8e3bd4221
pbain63 <prodipbain63@gmail.com>
2025-12-12 22:48:24 +0600

    Add toggle classlist visible in main.js

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ const dropdownContainer = document.querySelector(".dropdown-container");
 const menuTitle = document.querySelector(".menu-title");
 const navList = document.querySelector(".nav__list");
 
-menuTitle.addEventListener("click", (e) => {});
+menuTitle.addEventListener("click", (e) => {
+  if (e.target === e.currentTarget) {
+    navList.classList.toggle("visible");
+  }
+});
