9569e3ed97e8a96903656f24b68531a31b3d49ee
pbain63 <prodipbain63@gmail.com>
2025-12-12 22:45:03 +0600

    Add event listener to menuTitle in main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 const dropdownContainer = document.querySelector(".dropdown-container");
 const menuTitle = document.querySelector(".menu-title");
 const navList = document.querySelector(".nav__list");
+
+menuTitle.addEventListener("click", (e) => {});
