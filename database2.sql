0f351831fc7b985445e3ab0b9540be5f9d9c999c
pbain63 <prodipbain63@gmail.com>
2026-01-15 15:09:13 +0600

    Add db query for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,5 +1,13 @@
--- 13
-SELECT winner, yr, subject
-FROM nobel
-WHERE winner LIKE "Sir%"
-ORDER BY yr DESC, winner ASC;
+-- 14
+select
+    winner
+    , subject
+from nobel
+where yr = 1984
+order by
+    case
+        when subject in ('Physics', 'Chemistry') then 1
+        else 0
+    end
+    , subject
+    , winner
