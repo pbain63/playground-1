639d2c2d4e9a2ae370e78254aa24e4b8462d49ca
pbain63 <prodipbain63@gmail.com>
2026-01-16 14:44:50 +0600

    ADD Subquery with IN for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,5 +1,6 @@
---> 1
-SELECT name, gdp_per_capita
-FROM (SELECT name, gdp/population AS gdp_per_capita
-    FROM world) X
-WHERE gdp_per_capita > 20000;
+--> 2
+SELECT name
+  FROM world
+ WHERE continent IN
+  (SELECT continent FROM world
+    WHERE name='Bhutan')
