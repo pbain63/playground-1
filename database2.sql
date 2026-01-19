dc19ebe647571f0353b169c0f9be9d19879d43cb
pbain63 <prodipbain63@gmail.com>
2026-01-19 21:35:04 +0600

    Add db query with 'Using SELECT within SELECT' for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,11 +1,12 @@
 /*
 Q.2.
-List each country name where the population is larger 
-than that of 'Russia'.
-
+Show the countries in Europe with a per capita GDP 
+greater than 'United Kingdom'.
 */
 
-SELECT name
+SELECT name 
 FROM world
-WHERE population > (SELECT population from world
- WHERE name = 'Russia')
+WHERE continent = 'Europe' AND gdp/ population >
+(SELECT gdp/ population
+FROM world
+WHERE name = 'United Kingdom')
