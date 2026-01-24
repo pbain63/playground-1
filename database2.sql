763a21b3cbfa90fcbce8ac5f20e195a98d9e481f
pbain63 <prodipbain63@gmail.com>
2026-01-24 22:34:47 +0600

    Add db query for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,13 +1,7 @@
 /*
-Q.7.
-Show the name and population for each country with 
-a population of more than 100000000. Show countries
- in descending order of population.
+Q.1.
+Show the total population of the world.
  */
 
-
-SELECT name, population
-  FROM world
-  WHERE population > 100000000
-  ORDER BY population DESC
-
+SELECT SUM(population)
+FROM world
