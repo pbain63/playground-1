f3bc65719a49e126a2e263d50d60ccade3dfd327
pbain63 <prodipbain63@gmail.com>
2026-01-25 16:21:19 +0600

    Add db query with 'Using SELECT within SELECT' for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,14 +1,13 @@
 /*
-Q.9.
-Find the continents where all countries have a 
-population <= 25000000. Then find the names of 
-the countries associated with these continents. 
-Show name, continent and population.
- */
+Q.10.
+Some countries have populations more than three times 
+that of all of their neighbours (in the same continent). 
+Give the countries and continents. 
+*/
 
-SELECT x.name, x.continent, x.population
+SELECT x.name, x.continent
 FROM world x
-WHERE 25000000 >= ALL (SELECT y.population 
-                       FROM world y
-                       WHERE x.continent = y.continent
-                       AND population > 0);
+WHERE  x.population >= ALL (SELECT y.population *3 
+    FROM world y WHERE x.continent = y.continent 
+    AND x.name <> y.name)
+            
