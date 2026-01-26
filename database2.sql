4e86ee0a3fcba289e06bf147487667df309af01e
pbain63 <prodipbain63@gmail.com>
2026-01-26 13:12:07 +0600

    Add db query with 'Using SELECT within SELECT' quiz for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,12 +1,9 @@
 /*
-Q.1.
-Select the code that shows the name, region and 
-population of the smallest country in each region.
-
+Q.
+2. Select the code that shows the countries belonging 
+to regions with all populations over 50000
 */
 
-SELECT region, name, population FROM bbc x WHERE 
-population <= ALL (SELECT population FROM bbc y 
-        WHERE y.region=x.region AND population>0)
-
-            
+SELECT name,region,population FROM bbc x 
+WHERE 50000 < ALL (SELECT population FROM bbc y 
+    WHERE x.region=y.region AND y.population>0)
