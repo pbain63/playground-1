f8d47b125fcd944eb18492a88e90d31573d3e938
pbain63 <prodipbain63@gmail.com>
2026-01-30 19:20:06 +0600

    Add db query Using GROUP BY for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,17 +1,13 @@
 /*
 Q.
-5.
-GROUP BY and HAVING. The HAVING clause is tested 
-after the GROUP BY. You can test the aggregated 
-values with a HAVING clause. Show the total 
-population of those continents with a total 
-population of at least half a billion.
+6.
+For each continent show the continent and number of 
+countries.
 
 */
 
-SELECT continent, SUM(population)
-  FROM world
- GROUP BY continent
-HAVING SUM(population)>500000000
+SELECT continent, COUNT(name)
+FROM world
+GROUP BY continent;
 
 
