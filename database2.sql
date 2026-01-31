59c6e86442c0f953fc14b38d9278f42070ed5baa
pbain63 <prodipbain63@gmail.com>
2026-01-31 08:52:52 +0600

    Add db query Using Aggregate functions  for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,14 +1,10 @@
 /*
 Q.
-8.
-List the continents that have a total population of 
-at least 100 million.
+1.
+Show the total number of prizes awarded.
 
 */
 
-SELECT continent
-FROM world
-GROUP BY continent
-HAVING SUM(population) >= 100000000;
+SELECT COUNT(winner) FROM nobel
 
 
