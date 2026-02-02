7e2669006dc0b2d684e37dde0b365b22feea6269
pbain63 <prodipbain63@gmail.com>
2026-02-02 11:04:43 +0600

    Add db query Using JOIN operation for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,20 +1,16 @@
 /*
 Q.
-13.
-List every match with the goals scored by each team as
- shown. This will use "CASE WHEN" which has not been
-  explained in any previous exercises.
-d.
+12.
+For every match where 'GER' scored, show matchid, 
+match date and the number of goals scored by 'GER'
 
 
 */
 
-SELECT game.mdate, game.team1, 
-SUM(CASE WHEN goal.teamid = game.team1 THEN 1 ELSE 0 END) AS score1,
-game.team2,
-SUM(CASE WHEN goal.teamid = game.team2 THEN 1 ELSE 0 END) AS score2
-
-FROM game LEFT JOIN goal ON matchid = id
-GROUP BY game.id,game.mdate, game.team1, game.team2 
-ORDER BY mdate,matchid,team1,team2;
+SELECT matchid, mdate, COUNT(player)
+FROM goal
+  JOIN game
+    ON goal.matchid = game.id
+  WHERE teamid = 'GER'
+  GROUP BY matchid, mdate
 
