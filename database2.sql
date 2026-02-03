15c8f10bb2e0b6b689a0fe3bc8403dc89ef7637e
pbain63 <prodipbain63@gmail.com>
2026-02-03 21:01:57 +0600

    Add db query Using JOIN operation for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,16 +1,17 @@
 /*
 Q.
-14.
-List the films released in the year 1978 ordered by the number of actors in the cast, then by title.
+13.
+Obtain a list, in alphabetical order, of actors who've had at least 15 starring roles.
+
 
 
 
 */
 
-SELECT title, COUNT(actorid)
+SELECT name
 FROM actor 
   JOIN casting ON actor.id = casting.actorid
   JOIN movie ON movie.id = casting.movieid  
-WHERE yr = 1978
-GROUP BY title
-ORDER BY COUNT(actorid) DESC, title ASC;
+WHERE ord = 1
+GROUP BY name
+HAVING COUNT(movieid) >= 15
