e93a911d9a4e8bf2d4131207c86a645b40e94ea4
pbain63 <prodipbain63@gmail.com>
2026-02-03 21:02:51 +0600

    Add db query Using JOIN operation for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,17 +1,23 @@
 /*
 Q.
-13.
-Obtain a list, in alphabetical order, of actors who've had at least 15 starring roles.
+12.
+List the film title and the leading actor for all of the films 'Julie Andrews' played in.
+
+Did you get "Little Miss Marker twice"?
+Julie Andrews starred in the 1980 remake of Little Miss Marker and not the original(1934).
+
+Title is not a unique field, create a table of IDs in your subquery
 
 
 
 
 */
 
-SELECT name
-FROM actor 
-  JOIN casting ON actor.id = casting.actorid
-  JOIN movie ON movie.id = casting.movieid  
-WHERE ord = 1
-GROUP BY name
-HAVING COUNT(movieid) >= 15
+SELECT title, name
+  FROM movie JOIN casting ON (movieid = movie.id                                               AND ord = 1)
+             JOIN actor ON (actorid = actor.id)
+  WHERE movie.id IN (
+     SELECT movieid FROM casting
+        WHERE actorid IN (
+          SELECT id FROM actor
+            WHERE name = 'Julie Andrews'))
