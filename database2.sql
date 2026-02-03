d8bdcf194283139241a134382545efe6fdc1ec39
pbain63 <prodipbain63@gmail.com>
2026-02-03 20:59:52 +0600

    Add db query Using JOIN operation for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,15 +1,17 @@
 /*
 Q.
-10.
-Show the stadium and the number of goals scored in each 
-stadium.
+15.
+List all the people who have worked with 'Art Garfunkel'.
+
 
 
 */
 
-SELECT stadium, COUNT(player)
-FROM game
-  JOIN goal
-    ON game.id = goal.matchid
-  GROUP BY stadium;
-
+SELECT DISTINCT actor.name
+FROM movie
+JOIN casting
+ON casting.movieid = movie.id
+JOIN actor
+ON actor.id = casting.actorid
+WHERE movie.id IN (SELECT movieid FROM casting JOIN actor ON id =actorid WHERE 
+actor.name = 'Art Garfunkel') AND actor.name <> 'Art Garfunkel'
