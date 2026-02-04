b42c773abe949cbb22774dd62f0d0b93e18ba495
pbain63 <prodipbain63@gmail.com>
2026-02-04 18:45:06 +0600

    Add db query Using JOIN operation for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,23 +1,14 @@
 /*
 Q.
-12.
-List the film title and the leading actor for all of the films 'Julie Andrews' played in.
-
-Did you get "Little Miss Marker twice"?
-Julie Andrews starred in the 1980 remake of Little Miss Marker and not the original(1934).
-
-Title is not a unique field, create a table of IDs in your subquery
+7.
+A list of parties with the number of MSPs; include parties with no MSPs.
 
 
 
 
 */
 
-SELECT title, name
-  FROM movie JOIN casting ON (movieid = movie.id                                               AND ord = 1)
-             JOIN actor ON (actorid = actor.id)
-  WHERE movie.id IN (
-     SELECT movieid FROM casting
-        WHERE actorid IN (
-          SELECT id FROM actor
-            WHERE name = 'Julie Andrews'))
+SELECT party.Name, COUNT(msp.Name)
+FROM msp RIGHT JOIN party ON (msp.party = party.code)
+
+GROUP BY party.Name;
