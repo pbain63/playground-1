6c4f4b0478fb817c762bcf882a507fdcc8b543a6
pbain63 <prodipbain63@gmail.com>
2026-02-04 18:45:48 +0600

    Add db query Using JOIN operation for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,14 +1,13 @@
 /*
 Q.
-7.
-A list of parties with the number of MSPs; include parties with no MSPs.
-
+6.
+Obtain a list of parties which have MSPs, include the number of MSPs.
 
 
 
 */
 
 SELECT party.Name, COUNT(msp.Name)
-FROM msp RIGHT JOIN party ON (msp.party = party.code)
-
+FROM msp LEFT JOIN party ON (msp.party = party.code)
+WHERE party.Name IS NOT NULL
 GROUP BY party.Name;
