3820d3f6a34169d1d1978339ec136ae944c079b2
pbain63 <prodipbain63@gmail.com>
2026-02-06 23:52:38 +0600

    Add db query Using Self JOIN operation for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,17 +1,15 @@
 /*
 Q.
-1.
-The example uses a WHERE clause to show the cases in 'Italy' in March 2020.
+2.
+The LAG function is used to show data from the preceding row or the table. When lining up rows the data is partitioned by country name and ordered by the data whn. That means that only data from Italy is considered.
 
-Modify the query to show data from Spain
-
+Modify the query to show confirmed for the day before.
 
 */
 
-SELECT name, DAY(whn),
- confirmed, deaths, recovered
+SELECT name, DAY(whn), confirmed,
+   LAG(confirmed, 1) OVER (PARTITION BY name ORDER BY whn)
  FROM covid
-WHERE name = 'Spain'
+WHERE name = 'Italy'
 AND MONTH(whn) = 3 AND YEAR(whn) = 2020
 ORDER BY whn
-
