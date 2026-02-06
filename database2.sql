6db5c4245a9e6ddb0bbd783bce14360527b3aeb7
pbain63 <prodipbain63@gmail.com>
2026-02-06 23:50:50 +0600

    Add db query Using Self JOIN operation for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,17 +1,17 @@
 /*
 Q.
-6.
-The query shown is similar to the previous one, however by joining two copies of the stops table we can refer to stops by name rather than by number. Change the query so that the services between 'Craiglockhart' and 'London Road' are shown. If you are tired of these places try 'Fairmilehead' against 'Tollcross'
+1.
+The example uses a WHERE clause to show the cases in 'Italy' in March 2020.
+
+Modify the query to show data from Spain
+
 
 */
 
-
-SELECT a.company, a.num, stopa.name, stopb.name
-FROM route a JOIN route b ON
-  (a.company=b.company AND a.num=b.num)
-  JOIN stops stopa ON (a.stop=stopa.id)
-  JOIN stops stopb ON (b.stop=stopb.id)
-WHERE stopa.name='Craiglockhart' AND stopb.name='London Road';
-
-
+SELECT name, DAY(whn),
+ confirmed, deaths, recovered
+ FROM covid
+WHERE name = 'Spain'
+AND MONTH(whn) = 3 AND YEAR(whn) = 2020
+ORDER BY whn
 
