1ac4b48cfe0648e390b0ca98b9309f048caf8fc8
pbain63 <prodipbain63@gmail.com>
2026-02-07 21:50:00 +0600

    Add db query Using Self JOIN operation for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,13 +1,11 @@
 /*
 Q.
-8.
-Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross'
+7.
+Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
 
 */
 
 SELECT DISTINCT a.company, a.num
 FROM route a
-  JOIN route b ON (a.num=b.num AND a.company=b.company)
-  JOIN stops stopa ON (a.stop=stopa.id)
-  JOIN stops stopb ON (b.stop=stopb.id)
-WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'Tollcross'
+JOIN route b ON (a.company = b.company AND a.num = b.num)
+WHERE a.stop = 115 AND b.stop = 137;
