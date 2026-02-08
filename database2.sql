19a5fd2c76b79c35d54963b0c8a9e6e4193950d8
pbain63 <prodipbain63@gmail.com>
2026-02-08 21:31:29 +0600

    Add db query Using Self JOIN operation for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,14 +1,29 @@
 /*
 Q.
-9.
-Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself, offered by the LRT company. Include the company and bus no. of the relevant services.
+10.
+Find the routes involving two buses that can go from Craiglockhart to Lochend.
+Show the bus no. and company for the first bus, the name of the stop for the transfer,
+and the bus no. and company for the second bus.
 
 */
 
-SELECT DISTINCT bstop.name, a.company, a.num FROM
-	route AS a JOIN route AS b ON (a.company = b.company AND a.num = b.num)
-			   JOIN stops AS astop ON (a.stop = astop.id)
-			   JOIN stops AS bstop ON (b.stop = bstop.id)
-	WHERE astop.name = 'Craiglockhart'
+SELECT S.num, S.company, S.name, T.num, T.company 
+FROM 
+    (SELECT DISTINCT a.num, a.company, sb.name 
+     FROM route a JOIN route b ON (a.num = b.num and a.company = b.company) 
+                  JOIN stops sa ON sa.id = a.stop 
+                  JOIN stops sb ON sb.id = b.stop 
+     WHERE sa.name = 'Craiglockhart' AND sb.name <> 'Craiglockhart'
+)S
 
+JOIN
 
+    (SELECT x.num, x.company, sy.name 
+     FROM route x JOIN route y ON (x.num = y.num and x.company = y.company) 
+                  JOIN stops sx ON sx.id = x.stop 
+                  JOIN stops sy ON sy.id = y.stop 
+     WHERE sx.name = 'Lochend' AND sy.name <> 'Lochend'
+    )T
+
+ON (S.name = T.name)
+ORDER BY S.num, S.name, T.num
