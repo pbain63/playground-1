739dcc5ab15dc9add15d89fba8d88ee59333daf4
pbain63 <prodipbain63@gmail.com>
2026-02-08 21:21:24 +0600

    Add db query Using Self JOIN operation for practice in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,11 +1,14 @@
 /*
 Q.
-5.
-Execute the self join shown and observe that b.stop gives all the places you can get to from Craiglockhart, without changing routes. Change the query so that it shows the services from Craiglockhart to London Road.
+9.
+Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself, offered by the LRT company. Include the company and bus no. of the relevant services.
+
 */
 
-SELECT a.company, a.num, a.stop, b.stop
-FROM route a JOIN route b ON
-  (a.company=b.company AND a.num=b.num)
-WHERE a.stop=53 AND b.stop = 149
+SELECT DISTINCT bstop.name, a.company, a.num FROM
+	route AS a JOIN route AS b ON (a.company = b.company AND a.num = b.num)
+			   JOIN stops AS astop ON (a.stop = astop.id)
+			   JOIN stops AS bstop ON (b.stop = bstop.id)
+	WHERE astop.name = 'Craiglockhart'
+
 
