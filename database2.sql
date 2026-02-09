faf33f1e1d909d6253934bd385c80d6076a27cfd
pbain63 <prodipbain63@gmail.com>
2026-02-09 22:26:01 +0600

    Remove code in database2.sql

--- database2.sql
+++ database2.sql
@@ -1,29 +1,5 @@
 /*
 Q.
-10.
-Find the routes involving two buses that can go from Craiglockhart to Lochend.
-Show the bus no. and company for the first bus, the name of the stop for the transfer,
-and the bus no. and company for the second bus.
+
 
 */
-
-SELECT S.num, S.company, S.name, T.num, T.company 
-FROM 
-    (SELECT DISTINCT a.num, a.company, sb.name 
-     FROM route a JOIN route b ON (a.num = b.num and a.company = b.company) 
-                  JOIN stops sa ON sa.id = a.stop 
-                  JOIN stops sb ON sb.id = b.stop 
-     WHERE sa.name = 'Craiglockhart' AND sb.name <> 'Craiglockhart'
-)S
-
-JOIN
-
-    (SELECT x.num, x.company, sy.name 
-     FROM route x JOIN route y ON (x.num = y.num and x.company = y.company) 
-                  JOIN stops sx ON sx.id = x.stop 
-                  JOIN stops sy ON sy.id = y.stop 
-     WHERE sx.name = 'Lochend' AND sy.name <> 'Lochend'
-    )T
-
-ON (S.name = T.name)
-ORDER BY S.num, S.name, T.num
