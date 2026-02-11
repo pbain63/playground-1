5b3666052de5bb103bf199749f96bac2f445f235
pbain63 <prodipbain63@gmail.com>
2026-02-11 09:58:16 +0600

    Add JS code for practice in main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+const myObject = {
+    property: "Value",
+    otherProperty: 77,
+    "obnoxious property": function () {
+        // do stuff!
+    }
+}
+
+console.log(myObject.property);
+console.log(myObject["obnoxious property"]);
