8b32fc23c8ccdce968046d243352a96190a50177
pbain63 <prodipbain63@gmail.com>
2026-02-12 14:27:16 +0600

    Add JS code for practice in main.js

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-function gameOver(winningPlayer) {
-    console.log("Congratulations!");
-    console.log(`${winningPlayer.name} (${winningPlayer.maker}) is the winner!`);
-}
+const car = {
+    make: "Volkswagen",
+    model: "Golf",
+    year: 2026,
+    color: "blue",
+    priceUSD: 40000,
+
+
+    applyDiscount: function (discountPercentage) {
+        const multiplier = 1 - discountPercentage / 100;
+        this.priceUSD *= multiplier;
+    },
+
+    getSummary() {
+        return `${this.year} ${this.make} ${this.model} in ${this.color}, priced at $${this.priceUSD} (USD).`;
+    },
+
+
+};
