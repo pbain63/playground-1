94e6275a0fab5aadffb5f39b273a709fba94aa49
pbain63 <prodipbain63@gmail.com>
2026-02-13 22:15:03 +0600

    Add JS code for getters, setters practice in main.js

--- main.js
+++ main.js
@@ -1,28 +1,9 @@
-const rps = {
-    playerScore: 0,
-    computerScore: 0,
-    playRound(playerChoice) {
-         // code to play the round, update score if needed, then return the result
-    },
+let user = {
+    name: "John",
+    surName: "Smith",
 
-    getWinningPlayer() {
-        // return the player with the most points ("player", "computer", or "tie")
-    },
-
-    reset() {
-        // reset both players' scores to 0
-    },
+    get fullName() {
+        return `${this.name} ${this.surName}`;
+    }
 };
-rps.playRound("rock");
-console.log(rps.playerScore);
-
-rps.playRound("rock");
-console.log(rps.computerScore);
-
-rps.playRound("scissors");
-console.log(rps.playerScore);
-console.log(rps.getWinningPlayer());
-
-rps.reset();
-console.log(rps.playerScore);
-console.log(rps.computerScore);
+console.log(user.fullName);
