9028ab89eeb518aff799f3067be1b5a7f28d97b8
pbain63 <prodipbain63@gmail.com>
2026-02-13 22:20:54 +0600

    Add JS code for getters, setters practice in main.js

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ let user = {
 
     get fullName() {
         return `${this.name} ${this.surName}`;
+    },
+
+    set fullName(value) {
+        [this.name, this.name] = value.split(" ");
     }
 };
+user.fullName = "Alice Cooper";
+console.log(user.name);
+console.log(user.surName);
 console.log(user.fullName);
