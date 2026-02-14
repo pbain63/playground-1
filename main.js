673d89684339aadc15396a3fcbf5771d404f9a64
pbain63 <prodipbain63@gmail.com>
2026-02-14 21:10:35 +0600

    Add JS code for Smarter getters/setters practice in main.js

--- main.js
+++ main.js
@@ -1,22 +1,17 @@
 let user = {
-    name: "John",
-    surName: "Smith"
-};
-
-Object.defineProperty(user, 'fullName', {
-    get () {
-        return `${this.name} ${this.surName}`;
+    get name() {
+        return this._name;
     },
 
-    set (value) {
-        [this.name, this.name] = value.split(" ");
+    set name(value) {
+        if (value.length < 4) {
+            console.log("Name is too short, need at least 4 characters.");
+            return;
+        }
+        this._name = value;
     }
-})
-    
-
-user.fullName = "Alice Cooper";
+};
+user.name = "Pete";
 console.log(user.name);
-console.log(user.surName);
-console.log(user.fullName);
 
-for(let key in user) alert(key);
+user.name = "";
