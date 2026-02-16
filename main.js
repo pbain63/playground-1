32cc797d84af05c8afbf11fdebbeb99c2107faf0
pbain63 <prodipbain63@gmail.com>
2026-02-16 10:12:51 +0600

    Add JS code for class practice in main.js

--- main.js
+++ main.js
@@ -1,17 +1,18 @@
-class User {
-    constructor(name) {
-        this.name = name;
-    }
+// rewriting class User in pure functions
 
-    sayHi() {
-        alert(this.name);
-    }
-}
-
-alert(typeof User);
-
-alert(User = User.prototype.constructor);
-
-alert(User.prototype.sayHi);
-
-alert(Object.getOwnPropertyNames(User.prototype));
+// 1. Create constructor function
+function User(name) {
+    this.name = name;
+  }
+  // a function prototype has "constructor" property by default,
+  // so we don't need to create it
+  
+  // 2. Add the method to prototype
+  User.prototype.sayHi = function() {
+    alert(this.name);
+  };
+  
+  // Usage:
+  let user = new User("John");
+  user.sayHi();
+  
