f3d01bb742ee331f733636a8dd842c28e1cc9c53
pbain63 <prodipbain63@gmail.com>
2026-02-17 22:54:21 +0600

    Add JS code for class practice in main.js

--- main.js
+++ main.js
@@ -3,4 +3,23 @@ class Rectangle {
         this.height = height;
         this.width = width;
     }
+
+    get area() {
+        return this.calcArea();
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
+
+    *getSides() {
+        yield this.height;
+        yield this.width;
+        yield this.height;
+        yield this.width;
+    }
 }
+const square = new Rectangle(10, 10);
+console.log(square.area);
+console.log([...square.getSides()]);
+
