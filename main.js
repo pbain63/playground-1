29ce52da9c053e349d4ee63ac9c6e1d158a0fdc0
pbain63 <prodipbain63@gmail.com>
2026-02-18 21:19:45 +0600

    Add JS code for class practice in main.js

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
-class Rectangle {
-    constructor(height, width) {
-        this.height = height;
-        this.width = width;
+
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
     }
 
-    get area() {
-        return this.calcArea();
-    }
+    static displayName = "Point";
+    static distance(a, b) {
+        const dx = a.x - b.x;
+        const dy = a.y - b.y;
 
-    calcArea() {
-        return this.height * this.width;
-    }
-
-    *getSides() {
-        yield this.height;
-        yield this.width;
-        yield this.height;
-        yield this.width;
+        return Math.hypot(dx, dy);
     }
 }
-const square = new Rectangle(10, 10);
-console.log(square.area);
-console.log([...square.getSides()]);
 
+const p1 = new Point(5, 5);
+const p2 = new Point(10, 10);
+p1.displayName;
+p1.distance;
+p2.displayName;
+p2.distance;
+
+console.log(Point.displayName);
+console.log(Point.distance(p1, p2));
