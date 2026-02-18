c0099d7de635f4d80190a98d8a927744e2d749b3
pbain63 <prodipbain63@gmail.com>
2026-02-18 21:52:09 +0600

    Add JS code for class practice in main.js

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
-
-
-class Point {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
+class Animal {
+    constructor(name) {
+        this.name = name;
     }
 
-    static displayName = "Point";
-    static distance(a, b) {
-        const dx = a.x - b.x;
-        const dy = a.y - b.y;
-
-        return Math.hypot(dx, dy);
+    speak() {
+        console.log(`${this.name} makes a noise.`);
     }
 }
 
-const p1 = new Point(5, 5);
-const p2 = new Point(10, 10);
-p1.displayName;
-p1.distance;
-p2.displayName;
-p2.distance;
+class Dog extends Animal {
+    constructor(name) {
+        super(name);
+    }
 
-console.log(Point.displayName);
-console.log(Point.distance(p1, p2));
+    speak() {
+        console.log(`${this.name} barks.`);
+    }
+}
+const d = new Dog("Mitzie");
+d.speak();
