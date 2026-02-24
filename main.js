800f45e0766432eb4ec4c0868b8587f6e751f455
pbain63 <prodipbain63@gmail.com>
2026-02-24 22:52:11 +0600

    Add JS code for practice private elements in main.js

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 class C {
   #x;
-
+  constructor(x) {
+    this.#x = x;
+  }
   static getX(obj) {
-    return obj.#x;
+    if (#x in obj ) return obj.#x; 
+    return "obj must be an instance of C";
   }
 }
-
-console.log(C.getX(new C())); // undefined
-console.log(C.getX({})); // TypeError: Cannot read private member #x from an object whose class did not declare it
+console.log(C.getX(new C("foo")));
+console.log(C.getX(new C(0.196)));
+console.log(C.getX(new C(new Date())));
+console.log(C.getX({}));
