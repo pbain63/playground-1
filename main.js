fb36ac01a3ad5ddb9539435d3840d597ac754ef3
pbain63 <prodipbain63@gmail.com>
2026-02-27 23:28:01 +0600

    Add JS code for practice private elements in main.js

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-
 class ClassWithPrivateStaticField {
   static #privateStaticField = 42;
 
   static publicStaticMethod() {
+    // When invoked through super, `this` still refers to Subclass
     return this.#privateStaticField;
   }
 }
 
-class Subclass extends ClassWithPrivateStaticField {}
+class Subclass extends ClassWithPrivateStaticField {
+  static callSuperMethod() {
+    return super.publicStaticMethod();
+  }
+}
 
-Subclass.publicStaticMethod(); // TypeError: Cannot read private member #privateStaticField from an object whose class did not declare it
+Subclass.callSuperMethod(); // TypeError: Cannot read private member #privateStaticField from an object whose class did not declare it
