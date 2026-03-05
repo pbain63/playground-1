0f37e27f3b7fc463b2db698759a82082b6519a68
pbain63 <prodipbain63@gmail.com>
2026-03-05 22:40:04 +0600

    Uodate JS code for practice private elements in main.js

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-class ClassWithPrivateMethod {
-  #privateMethod() {
-    return 42;
+class ClassWithPrivateAccessor {
+  #message;
+
+  get #decoratedMessage() {
+    return `🎬${this.#message}🛑`;
+  }
+  set #decoratedMessage(msg) {
+    this.#message = msg;
   }
 
-  publicMethod() {
-    return this.#privateMethod();
+  constructor() {
+    this.#decoratedMessage = "hello world";
+    console.log(this.#decoratedMessage);
   }
 }
 
-const instance = new ClassWithPrivateMethod();
-console.log(instance.publicMethod()); // 42
+new ClassWithPrivateAccessor(); // 🎬hello world🛑
