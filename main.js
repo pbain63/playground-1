6d385077d6a45cfdae6c1ea95521a21bc20e71c8
pbain63 <prodipbain63@gmail.com>
2026-03-08 21:58:27 +0600

    Uodate JS code for practice static in main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+class ClassWithStaticMethod {
+  static staticProperty = "someValue";
+  static staticMethod() {
+    return "static method has been called.";
+  }
+  static {
+    console.log("Class static initialization block called");
+  }
+}
+
+console.log(ClassWithStaticMethod.staticProperty);
+// Expected output: "someValue"
+console.log(ClassWithStaticMethod.staticMethod());
+// Expected output: "static method has been called."
