f156c791a00162e3c48d0b6d9139b3b393410218
pbain63 <prodipbain63@gmail.com>
2026-03-09 21:30:28 +0600

    Update JS code for practice static in main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+class ClassWithStaticField {
+  static baseStaticField = "base static field";
+  static anotherBaseStaticField = this.baseStaticField;
+
+  static baseStaticMethod() {
+    return "base static method output";
+  }
+}
+
+class SubClassWithStaticField extends ClassWithStaticField {
+  static subStaticField = super.baseStaticMethod();
+}
+
+console.log(ClassWithStaticField.anotherBaseStaticField); // "base static field"
+console.log(SubClassWithStaticField.subStaticField); // "base static method output"
