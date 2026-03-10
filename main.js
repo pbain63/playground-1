04533b5be776165093994b56e9f1fddfc5650031
pbain63 <prodipbain63@gmail.com>
2026-03-10 20:34:26 +0600

    Update JS code for practice static in main.js

--- main.js
+++ main.js
@@ -1,29 +1,14 @@
-class Triple {
-  static customName = "Tripler";
-  static description = "I triple any number you provide";
-  static calculate(n = 1) {
-    return n * 3;
+class StaticMethodCall {
+  static staticProperty = "static property";
+  static staticMethod() {
+    return `Static method and ${this.staticProperty} has been called`;
+  }
+  static anotherStaticMethod() {
+    return `${this.staticMethod()} from another static method`;
   }
 }
+StaticMethodCall.staticMethod();
+// 'Static method and static property has been called'
 
-class SquaredTriple extends Triple {
-  static longDescription;
-  static description = "I square the triple of any number you provide";
-  static calculate(n) {
-    return super.calculate(n) * super.calculate(n);
-  }
-}
-
-console.log(Triple.description); // 'I triple any number you provide'
-console.log(Triple.calculate()); // 3
-console.log(Triple.calculate(6)); // 18
-
-const tp = new Triple();
-
-console.log(SquaredTriple.calculate(3)); // 81 (not affected by parent's instantiation)
-console.log(SquaredTriple.description); // 'I square the triple of any number you provide'
-console.log(SquaredTriple.longDescription); // undefined
-console.log(SquaredTriple.customName); // 'Tripler'
-
-// This throws because calculate() is a static member, not an instance member.
-console.log(tp.calculate()); // 'tp.calculate is not a function'
+StaticMethodCall.anotherStaticMethod();
+// 'Static method and static property has been called from another static method'
