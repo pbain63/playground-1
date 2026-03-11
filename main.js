b92afd6e51ab79a431fc2bdf64f28be1ac86a7e7
pbain63 <prodipbain63@gmail.com>
2026-03-11 14:01:27 +0600

    Add JS code for practice calling static members from a class constructor method in main.js

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 class StaticMethodCall {
+  constructor() {
+    console.log(StaticMethodCall.staticProperty); // 'static property'
+    console.log(this.constructor.staticProperty); // 'static property'
+    console.log(StaticMethodCall.staticMethod()); // 'static method has been called.'
+    console.log(this.constructor.staticMethod()); // 'static method has been called.'
+  }
+
   static staticProperty = "static property";
   static staticMethod() {
-    return `Static method and ${this.staticProperty} has been called`;
-  }
-  static anotherStaticMethod() {
-    return `${this.staticMethod()} from another static method`;
+    return "static method has been called.";
   }
 }
-StaticMethodCall.staticMethod();
-// 'Static method and static property has been called'
-
-StaticMethodCall.anotherStaticMethod();
-// 'Static method and static property has been called from another static method'
