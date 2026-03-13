78645b818d6d81146f81676aeaef9961ef2a1244
pbain63 <prodipbain63@gmail.com>
2026-03-13 22:11:42 +0600

    Add JS code for practice private elements in main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-class ClassWithPrivate {
-  #privateField;
-  #privateFieldWithInitializer = 42;
-
-  #privateMethod() {
-    // …
+class C {
+  #x;
+  constructor(x) {
+    this.#x = x;
   }
+  static getX(obj) {
+    if (#x in obj) return obj.#x;
 
-  static #privateStaticField;
-  static #privateStaticFieldWithInitializer = 42;
-
-  static #privateStaticMethod() {
-    // …
+    return "obj must be an instance of C";
   }
 }
+console.log(C.getX(new C("foo"))); // "foo"
+console.log(C.getX(new C(0.196))); // 0.196
+console.log(C.getX(new C(new Date()))); // the current date and time
+console.log(C.getX({})); // "obj must be an instance of C"
