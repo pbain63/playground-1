d619a978917ca142b6e0b00ecea43e89586e7aed
pbain63 <prodipbain63@gmail.com>
2026-03-15 22:10:42 +0600

    Add JS code for practice private elements in main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+class Stamper extends class {
+    // A base class whose constructor returns the object it's given
+    constructor(obj) {
+      return obj;
+    }
+  } {
+    // This declaration will "stamp" the private field onto the object
+    // returned by the base class constructor
+    #stamp = 42;
+    static getStamp(obj) {
+      return obj.#stamp;
+    }
+  }
+  
+  const obj = {};
+  new Stamper(obj);
+  // `Stamper` calls `Base`, which returns `obj`, so `obj` is
+  // now the `this` value. `Stamper` then defines `#stamp` on `obj`
+  
+  console.log(obj); // In some dev tools, it shows {#stamp: 42}
+  console.log(Stamper.getStamp(obj)); // 42
+  console.log(obj instanceof Stamper); // false
+  
+  // You cannot stamp private elements twice
+  new Stamper(obj); // Error: Initializing an object twice is an error with private fields
+  
