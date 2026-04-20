78bfb956a1e226f00533739e33155bff04d3fd5c
pbain63 <prodipbain63@gmail.com>
2026-04-20 09:34:31 +0600

    Add JS code for practice in main.js

--- main.js
+++ main.js
@@ -1,25 +1,8 @@
-class Stamper extends class {
-  // A base class whose constructor returns the object it's given
-  constructor(obj) {
-    return obj;
-  }
-} {
-  // This declaration will "stamp" the private field onto the object
-  // returned by the base class constructor
-  #stamp = 42;
-  static getStamp(obj) {
-    return obj.#stamp;
+class ClassWithPrivateStaticField {
+  static #privateStaticField = 42;
+
+  static publicStaticMethod() {
+    return ClassWithPrivateStaticField.#privateStaticField;
   }
 }
-
-const obj = {};
-new Stamper(obj);
-// `Stamper` calls `Base`, which returns `obj`, so `obj` is
-// now the `this` value. `Stamper` then defines `#stamp` on `obj`
-
-console.log(obj); // In some dev tools, it shows {#stamp: 42}
-console.log(Stamper.getStamp(obj)); // 42
-console.log(obj instanceof Stamper); // false
-
-// You cannot stamp private elements twice
-new Stamper(obj); // Error: Initializing an object twice is an error with private fields
+console.log(ClassWithPrivateStaticField.publicStaticMethod());
