331157ef7d81075892c379a9c81c463bd512f450
pbain63 <prodipbain63@gmail.com>
2026-04-24 21:38:30 +0600

    Add JS code for practice in main.js

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
-class ClassWithStaticField {
-  static baseStaticField = "Base static field";
-  static anotherBaseStaticField = this.baseStaticField;
-
-  static baseStaticMethod() {
-    return "Base static method output";
+class Triple {
+  static customName = "Tripler";
+  static description = "I triple any number you provide";
+  static calculate(n = 1) {
+    return n * 3; 
   }
 }
 
-class SubClassWithStaticField extends ClassWithStaticField {
-  static subStaticField = super.baseStaticMethod();
+class SquaredTriple extends Triple {
+  static longDescription;
+  static description = "I square the triple of any number you provide";
+  static calculate(n) {
+    return super.calculate(n) * super.calculate(n);
+  }
 }
 
-console.log(ClassWithStaticField.anotherBaseStaticField);
-console.log(SubClassWithStaticField.subStaticField);
+console.log(Triple.description);
+console.log(Triple.calculate());
+console.log(Triple.calculate(6));
+
+const tp = new Triple();
+
+console.log(SquaredTriple.calculate(3));
+console.log(SquaredTriple.description);
+console.log(SquaredTriple.longDescription);
+console.log(SquaredTriple.customName);
+
+console.log(tp.calculate(3));
