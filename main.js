20ff69f52d08d48e4cbf3ca6e162a4f6129634e6
pbain63 <prodipbain63@gmail.com>
2026-05-01 21:24:46 +0600

    Add JS code to practice default exports in main.js

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-function cube(x) {
+export default function cube(x) {
     return x * x * x;
 }
-
-const foo = Math.PI + Math.SQRT2;
-
-const graph = {
-    options: {
-        color: "white",
-        thickness: "2px",
-    },
-    draw() {
-        console.log("From graph draw function");
-        
-    },
-}
-
-export {cube, foo, graph };
