3cf66a490795761ca048fe859ea4ff0af8e12ad5
pbain63 <prodipbain63@gmail.com>
2026-05-01 21:17:23 +0600

    Add JS code to practice named exports in main.js

--- main2.js
+++ main2.js
@@ -1,2 +1,10 @@
-import m from "./main.js";
-console.log(m);
+import { cube, foo, graph } from "./main.js";
+
+graph.options = {
+    color: "blue",
+    thickness: "3px",
+};
+
+graph.draw();
+console.log(cube(3));
+console.log(foo);
