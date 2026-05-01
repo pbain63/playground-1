afc357c2b9683d3c28f639f8c476af17b3d99efd
pbain63 <prodipbain63@gmail.com>
2026-05-01 21:17:04 +0600

    Add JS code to practice named exports in main.js

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
-const k = 12;
-export default k;
+function cube(x) {
+    return x * x * x;
+}
+
+const foo = Math.PI + Math.SQRT2;
+
+const graph = {
+    options: {
+        color: "white",
+        thickness: "2px",
+    },
+    draw() {
+        console.log("From graph draw function");
+        
+    },
+}
+
+export {cube, foo, graph };
