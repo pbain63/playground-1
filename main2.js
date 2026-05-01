917baade4d41041c0ca3918cb6b5b67e194fda3f
pbain63 <prodipbain63@gmail.com>
2026-05-01 21:25:00 +0600

    Add JS code to practice default exports in main2.js

--- main2.js
+++ main2.js
@@ -1,10 +1,3 @@
-import { cube, foo, graph } from "./main.js";
+import cube from "./main.js";
 
-graph.options = {
-    color: "blue",
-    thickness: "3px",
-};
-
-graph.draw();
 console.log(cube(3));
-console.log(foo);
