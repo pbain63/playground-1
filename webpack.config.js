4cebfdb8bccd5d56e178668072d63c46eba24d5b
pbain63 <prodipbain63@gmail.com>
2026-05-13 21:20:06 +0600

    Add dummy code for practice in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,12 @@
 import path from "node:path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
-  mode: "development",
-  entry: "./src/index.js",
+  entry: "./path/to/my/entry/file.js",
   output: {
-    filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
-    clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        use: ["html-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      }
-    ],
+    path: path.resolve(__dirname, "dist"),
+    filename: "my-first-webpack.bundle.js",
   },
 };
