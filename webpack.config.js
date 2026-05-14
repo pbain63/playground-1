54061c1f2ef6372b0968425321e42284e96226f0
pbain63 <prodipbain63@gmail.com>
2026-05-14 21:38:26 +0600

    Add dummy code for practice loading in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 import path from "node:path";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export default {
-  entry: "./path/to/my/entry/file.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
     filename: "my-first-webpack.bundle.js",
+  },
+  module: {
+    rules: [{ test: /\.js$/, use: "babel-loader" }],
   },
 };
