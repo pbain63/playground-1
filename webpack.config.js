e41abfc0119328e1f251f67148816b6435961848
pbain63 <prodipbain63@gmail.com>
2026-05-16 09:34:54 +0600

    Add mode parameter for practice in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack"; // to access built-in plugins
 
 export default {
+  mode: "production",
   module: {
     rules: [{ test: /\.js$/, use: "babel-loader" }],
   },
