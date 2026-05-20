a7c8730207e0437501cea461c2fe775093bf356c
pbain63 <prodipbain63@gmail.com>
2026-05-20 20:37:15 +0600

    Add code for practice webpack in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,25 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.scss$/i,
-        use: ["postcss-loader", "sass-loader"],
-      }
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: "asset/resource",
+      },
     ],
   },
 };
