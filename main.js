a772db05aa5cba198c6655876308a75e54bfc476
pbain63 <prodipbain63@gmail.com>
2026-05-23 23:13:23 +0600

    Add JS code for practice webpack in main.js

--- main.js
+++ main.js
@@ -3,6 +3,18 @@ import "./style.css";
 import icon from "./icon.png";
 import Data from "./data.xml";
 import Notes from "./data.csv";
+import toml from "./data.toml";
+import yaml from "./data.yaml";
+import jason from "./data.jason5";
+
+console.log(toml.title);
+console.log(toml.owner.name);
+
+console.log(yaml.title);
+console.log(yaml.owner.name);
+
+console.log(jason.title);
+console.log(jason.owner.name);
 
 function component() {
   const element = document.createElement("div");
