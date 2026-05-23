012c4197eceb0f0de8c97819810c6e8e456314bf
pbain63 <prodipbain63@gmail.com>
2026-05-23 23:08:51 +0600

    Add code for practice webpack in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
-import path from "node:path";
+import path, { parse } from "node:path";
 import { fileURLToPath } from "node:url";
+import toml from "toml";
+import yaml from "yamljs";
+import json5 from "json5";
+import { type } from "node:os";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -31,6 +35,27 @@ export default {
       {
         test: /\.xml$/i,
         use: ["xml-loader"],
+      },
+      {
+        test: /\.toml$/i,
+        type: "json",
+        parser: {
+          parse: toml.parse,
+        },
+      },
+      {
+        test: /\.yaml$/i,
+        type: "json",
+        parser: {
+          parse: yaml.parse,
+        },
+      },
+      {
+        test: /\.jason5$/i,
+        type: "json",
+        parser: {
+          parse: jason5.parse,
+        },
       },
     ],
   },
