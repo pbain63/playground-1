8a02e545e668b30ac150944e056ae9088fdde68b
pbain63 <prodipbain63@gmail.com>
2026-07-17 20:20:17 +0600

    Add  code to practice JSON.parse() in main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-let myObj = {name: "Chris", age: 38};
-myObj;
-let myString = JSON.stringify(myObj);
-myString;
-// console.log(myString);//
+const obj = JSON.parse('{"name":"John", "age":30, "city":"New York"}');
+console.log(obj);
+
+console.log(obj.name);
+console.log(obj.age);
