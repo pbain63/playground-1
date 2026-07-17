48763837c8d2cdf60de0d7681be3149f609ebe68
pbain63 <prodipbain63@gmail.com>
2026-07-17 13:04:56 +0600

    Add parsed version of array to practice JSON in main.js

--- main.js
+++ main.js
@@ -1,36 +1,16 @@
-import _ from "lodash";
-import "./style.css";
-import icon from "./icon.png";
-import Data from "./data.xml";
-import Notes from "./data.csv";
-import toml from "./data.toml";
-import yaml from "./data.yaml";
-import jason from "./data.jason5";
+const superHeroes = [
+  {
+    name: "Molecule Man",
+    age: 29,
+    secretIdentity: "Dan Jukes",
+    powers: ["Radiation resistance", "Turning tiny", "Radiation blast"],
+  },
+  {
+    name: "Madame Uppercut",
+    age: 39,
+    secretIdentity: "Jane Wilson",
+    powers: ["Million tonne punch", "Damage resistance", "Superhuman reflexes"],
+  },
+];
 
-console.log(toml.title);
-console.log(toml.owner.name);
-
-console.log(yaml.title);
-console.log(yaml.owner.name);
-
-console.log(jason.title);
-console.log(jason.owner.name);
-
-function component() {
-  const element = document.createElement("div");
-
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  console.log(Data);
-  console.log(Notes);
-
-  return element;
-}
-
-document.body.appendChild(component());
+console.log(superHeroes[0].powers[0]); //
