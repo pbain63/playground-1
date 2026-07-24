f7c44224b35aa7a41fbca3d4402bba0e72135a98
pbain63 <prodipbain63@gmail.com>
2026-07-24 11:08:22 +0600

    Add JS code for practice object assign in main.js

--- main.js
+++ main.js
@@ -1,48 +1,7 @@
-function swimmer({ name }) {
-  return {
-    swim: () => console.log(`${name} swam`),
-  };
-}
+const original = { a: 1, b: 2 };
+const copy = { ...original, c: 3 }; // copy => { a: 1, b: 2, c: 3 }
 
-function attackerAndWalker({ name }) {
-  return {
-    attack: () => console.log(`${name} attacked`),
-    walk: () => console.log(`${name} walked`),
-  };
-}
+const { a, ...noA } = copy; // noA => { b: 2, c: 3 }
 
-function flyer({ name }) {
-  return {
-    fly: () => {
-      console.log(`${name} flew`);
-    },
-  };
-}
-
-function swimmingMonsterCreator(name) {
-  const monster = { name: name };
-
-  return {
-    ...monster,
-    ...attackerAndWalker(monster),
-    ...swimmer(monster),
-  };
-}
-
-function flyingSwimmingMonsterCreator(name) {
-  const monster = { name: name };
-
-  return {
-    ...monster,
-    ...attackerAndWalker(monster),
-    ...swimmer(monster),
-    ...flyer(monster),
-  };
-}
-
-const obj = flyingSwimmingMonsterCreator("Monster");
-
-obj.attack();
-obj.walk();
-obj.swim();
-obj.fly();
+console.log(copy);
+console.log(noA);
