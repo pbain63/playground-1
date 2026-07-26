f8b7bbdf78680b5bb528417da1806166c1dd5695
pbain63 <prodipbain63@gmail.com>
2026-07-26 15:40:29 +0600

    Add JS code for practice custom form validation to JS in main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const email = document.getElementById("mail");
 
 email.addEventListener("input", (event) => {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("Please enter a valid email address.");
-  } else {
-    email.setCustomValidity("");
+  email.setCustomValidity("");
+  if (!email.validity.valid) {
+    return;
+  }
+  if (!email.value.endsWith("@example.com")) {
+    email.setCustomValidity("Please enter an email address of @example.com");
   }
 });
