052adfdf5e902a593d9075b4a0c36cb99428832a
pbain63 <prodipbain63@gmail.com>
2026-07-26 10:25:30 +0600

    Add html code for practice form validation to JS in main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-const numbers = [1, 2, 3, 4, 5];
-const increasedByOne = numbers.map((num) => num + 1);
-console.log(increasedByOne);
-// console.log(noA);
+const email = document.getElementById("mail");
+
+email.addEventListener("input", (event) => {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("Please enter a valid email address.");
+  } else {
+    email.setCustomValidity("");
+  }
+});
