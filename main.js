5f58b660ffc9fcb7788dbb55604819b48fb0f96d
pbain63 <prodipbain63@gmail.com>
2026-07-27 15:28:25 +0600

    Add JS code for practice Limiting the size of a file before its upload in main.js

--- main.js
+++ main.js
@@ -1,44 +1,16 @@
-const countrySelect = document.getElementById("country");
-const postalCodeField = document.getElementById("postal-code");
+const fs = document.getElementById("fs");
 
-function checkPostalCode() {
-  
-  const constraints = {
-    ch: [
-      "^(CH-)?\\d{4}$",
-      "Swiss postal codes must have exactly 4 digits: e.g. CH-1950 or 1950",
-    ],
-    fr: [
-      "^(F-)?\\d{5}$",
-      "French postal codes must have exactly 5 digits: e.g. F-75012 or 75012",
-    ],
-    de: [
-      "^(D-)?\\d{5}$",
-      "German postal codes must have exactly 5 digits: e.g. D-12345 or 12345",
-    ],
-    nl: [
-      "^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
-      "Dutch postal codes must have exactly 4 digits, followed by 2 letters except SA, SD and SS",
-    ],
-  };
+function checkFileSize() {
+  const files = fs.files;
 
-  // Read the country id
-  const country = countrySelect.value;
-
-  // Build the constraint checker
-  const constraint = new RegExp(constraints[country][0], "");
-  console.log(constraint);
-
-  // Check it!
-  if (constraint.test(postalCodeField.value)) {
-    // The postal code follows the constraint, we use the ConstraintAPI to tell it
-    postalCodeField.setCustomValidity("");
-  } else {
-    // The postal code doesn't follow the constraint, we use the ConstraintAPI to
-    // give a message about the format required for this country
-    postalCodeField.setCustomValidity(constraints[country][1]);
+  if (files.length > 0) {
+    if (files[0].size > 75 * 1000) {
+      fs.setCustomValidity("The selected file must not be larger than 75 kB");
+      fs.reportValidity();
+      return;
+    }
   }
+  fs.setCustomValidity("");
 }
 
-countrySelect.addEventListener("change", checkPostalCode);
-postalCodeField.addEventListener("input", checkPostalCode);
+fs.addEventListener("change", checkFileSize);
