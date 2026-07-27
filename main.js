2844bda85611ed643382787c4f82a2e309500d4b
pbain63 <prodipbain63@gmail.com>
2026-07-27 15:11:11 +0600

    Add JS code for practice Constraint combining several fields in main.js

--- main.js
+++ main.js
@@ -1,44 +1,44 @@
-const form = document.querySelector("form");
-const email = document.getElementById("mail");
-const error = document.getElementById("error");
+const countrySelect = document.getElementById("country");
+const postalCodeField = document.getElementById("postal-code");
 
-const emailRegExp = /^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d-]+(?:\.[a-z\d-]+)*$/i;
+function checkPostalCode() {
+  
+  const constraints = {
+    ch: [
+      "^(CH-)?\\d{4}$",
+      "Swiss postal codes must have exactly 4 digits: e.g. CH-1950 or 1950",
+    ],
+    fr: [
+      "^(F-)?\\d{5}$",
+      "French postal codes must have exactly 5 digits: e.g. F-75012 or 75012",
+    ],
+    de: [
+      "^(D-)?\\d{5}$",
+      "German postal codes must have exactly 5 digits: e.g. D-12345 or 12345",
+    ],
+    nl: [
+      "^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
+      "Dutch postal codes must have exactly 4 digits, followed by 2 letters except SA, SD and SS",
+    ],
+  };
 
-const isValidEmail = () => {
-  const validity = email.value.length !== 0 && emailRegExp.test(email.value);
-  return validity;
-};
+  // Read the country id
+  const country = countrySelect.value;
 
-const setEmailClass = (isValid) => {
-  email.className = isValid ? "valid" : "invalid";
-};
+  // Build the constraint checker
+  const constraint = new RegExp(constraints[country][0], "");
+  console.log(constraint);
 
-const updateError = (isValid) => {
-  if (isValid) {
-    error.textContent = "";
-    error.removeAttribute("class");
+  // Check it!
+  if (constraint.test(postalCodeField.value)) {
+    // The postal code follows the constraint, we use the ConstraintAPI to tell it
+    postalCodeField.setCustomValidity("");
   } else {
-    error.textContent = "I expect an email, darling!";
-    error.setAttribute("class", "active");
+    // The postal code doesn't follow the constraint, we use the ConstraintAPI to
+    // give a message about the format required for this country
+    postalCodeField.setCustomValidity(constraints[country][1]);
   }
-};
+}
 
-const handleInput = () => {
-  const validity = isValidEmail();
-  setEmailClass(validity);
-  updateError(validity);
-};
-
-const handleSubmit = (event) => {
-  event.preventDefault();
-  const validity = isValidEmail();
-  setEmailClass(validity);
-  updateError(validity);
-};
-
-const validity = isValidEmail();
-setEmailClass(validity);
-
-email.addEventListener("input", handleInput);
-
-form.addEventListener("submit", handleSubmit);
+countrySelect.addEventListener("change", checkPostalCode);
+postalCodeField.addEventListener("input", checkPostalCode);
