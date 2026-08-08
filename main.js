b5997f328e66dba65ed7f07e58010a6d2d6a988c
pbain63 <prodipbain63@gmail.com>
2026-08-08 20:51:24 +0600

    Add JS code to practice Promise in main.js

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
-Promise.resolve().then(() => console.log(1));
-
-setTimeout(() => {
-  console.log(2);
-}, 10);
-
-queueMicrotask(() => {
-  console.log(3);
-  queueMicrotask(() => console.log(4));
+new Promise((resolve) => {
+  console.log(1);
+  resolve(2);
+}).then((result) => {
+  console.log(result);
 });
 
-console.log(5);
+console.log(3);
