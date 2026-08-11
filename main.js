a1fc85f11efa2620505b9fa38d8d0d4c16bb6557
pbain63 <prodipbain63@gmail.com>
2026-08-11 11:17:52 +0600

    Add JS code to practice fetch() in main.js

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
-function loadScript(src) {
-  return new Promise(function (resolve, reject) {
-    let script = document.createElement("script");
-    script.src = src;
-
-    script.onload = () => resolve(script);
-    script.onerror = () => reject(new Error(`Script load error for ${src}`));
-    document.head.append(script);
+fetch("https://url.com.some/url")
+  .then(function (response) {
+    // Successful response ;)
+  })
+  .catch(function (err) {
+    // Error :(
   });
-}
-
-let promise = loadScript(
-  "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.11/lodash.js"
-);
-promise.then(
-  (script) => console.log(`${script.src} is loaded!`),
-  (error) => console.log(`Error: ${error.message}`)
-);
-
-promise.then((script) => console.log("Another handler..."));
