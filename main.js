4e3222ebb2c655f5039b498c48d055671781dc74
pbain63 <prodipbain63@gmail.com>
2026-08-16 23:08:57 +0600

    Add Refactor previous promise code to async / await code in main.js

--- main.js
+++ main.js
@@ -1,30 +1,16 @@
-const server = {
-  people: [
-    { name: "Odin", age: 20 },
-    { name: "Thor", age: 35 },
-    { name: "Freyja", age: 29 },
-  ],
+const img = document.querySelector("img");
 
-  getPeople() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve(this.people);
-      }, 2000);
-    });
-  },
-};
-
-async function getPersonsInfo(name) {
+async function getCats() {
   try {
-    const people = await server.getPeople();
-    const person = people.find((person) => {
-      return person.name === name;
-    });
-    return person;
+    const response = await fetch(
+      "https://api.giphy.com/v1/gifs/translate?api_key=JxoghZbdsbGeUxaK82pDSgRxEX40cj8U&s=cats"
+    );
+    const catData = await response.json();
+
+    img.src = catData.data.images.fixed_width.webp;
+    console.log(img.src);
   } catch (error) {
     console.error(error);
   }
 }
-
-// getPersonsInfo();
-console.log(getPersonsInfo());
+getCats();
