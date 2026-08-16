6cb3091944272420101a77243d7bfd80247e2c99
pbain63 <prodipbain63@gmail.com>
2026-08-16 20:29:39 +0600

    Add JS code for practice Async and Await in main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+const server = {
+  people: [
+    { name: "Odin", age: 20 },
+    { name: "Thor", age: 35 },
+    { name: "Freyja", age: 29 },
+  ],
+
+  getPeople() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(this.people);
+      }, 2000);
+    });
+  },
+};
+
+async function getPersonsInfo(name) {
+  const people = await server.getPeople();
+  const person = people.find((person) => {
+    return person.name === name;
+  });
+  return person;
+}
+
+getPersonsInfo();
+console.log(getPersonsInfo());
