085c6a0388abbb6ddf3105ea7aefeddfe142634c
pbain63 <prodipbain63@gmail.com>
2026-08-17 22:22:39 +0600

    Add JS code to practice try-catch statement in main.js

--- main.js
+++ main.js
@@ -1,18 +1,12 @@
-class Thenable {
-  constructor(num) {
-    this.num = num;
-  }
-
-  then(resolve, reject) {
-    alert(resolve);
-    setTimeout(() => {
-      resolve(this.num * 2);
-    }, 1000);
-  }
-}
-
 async function f() {
-  let result = await new Thenable(5);
-  alert(result);
+  try {
+    let response = await fetch("https://no-such-url");
+  } catch (error) {
+    alert(error);
+    // console.log(error);
+    // console.error(error);
+    
+    
+  }
 }
 f();
