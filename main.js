39eaaff25a7737bf137ceea3703f84dcdcad8549
pbain63 <prodipbain63@gmail.com>
2026-08-17 15:23:12 +0600

    Format promises code to async/await in main.js

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
-async function f() {
-  let promise = new Promise((resolve, reejct) => {
-    setTimeout(() => resolve("done!"), 2000);
-  });
+async function showAvatar() {
 
-  let result = await promise;
+  // read our JSON
+  let response = await fetch('/article/promise-chaining/user.json');
+  let user = await response.json();
 
-  alert(result);
+  // read github user
+  let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
+  let githubUser = await githubResponse.json();
+
+  // show the avatar
+  let img = document.createElement('img');
+  img.src = githubUser.avatar_url;
+  img.className = "promise-avatar-example";
+  document.body.append(img);
+
+  // wait 3 seconds
+  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+
+  img.remove();
+
+  return githubUser;
 }
-f();
+
+showAvatar();
