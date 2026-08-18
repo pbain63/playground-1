1347bd9572255a8aa7dd23be8546ce80457c750e
pbain63 <prodipbain63@gmail.com>
2026-08-18 22:54:32 +0600

    Rewrite Promise code to async/await in main.js

--- main.js
+++ main.js
@@ -6,18 +6,25 @@ function doubleAfter2Seconds(x) {
   });
 }
 
-function addPromise(x) {
-  return new Promise((resolve) => {
-    doubleAfter2Seconds(10).then((a) => {
-      doubleAfter2Seconds(20).then((b) => {
-        doubleAfter2Seconds(30).then((c) => {
-          resolve(x + a + b + c);
-        });
-      });
-    });
-  });
+// function addPromise(x) {
+//   return new Promise((resolve) => {
+//     doubleAfter2Seconds(10).then((a) => {
+//       doubleAfter2Seconds(20).then((b) => {
+//         doubleAfter2Seconds(30).then((c) => {
+//           resolve(x + a + b + c);
+//         });
+//       });
+//     });
+//   });
+// }
+
+async function addSync(x) {
+  const a = await doubleAfter2Seconds(10);
+  const b = await doubleAfter2Seconds(20);
+  const c = await doubleAfter2Seconds(30);
+  return x + a + b + c;
 }
 
-addPromise(10).then((sum) => {
+addSync(10).then((sum) => {
   console.log(sum);
 });
