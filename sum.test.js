40ce3945af033b6c0c9f0cff5f86dfd15b3476f3
pbain63 <prodipbain63@gmail.com>
2026-08-21 14:46:18 +0600

    Add test for practice Truthiness in sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,23 @@
-const sum = require('./sum');
+// const sum = require("./sum");
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+// test("adds 1 + 2 to equal 3", () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+
+test("null", () => {
+  const n = null;
+  expect(n).toBeNull();
+  expect(n).toBeDefined();
+  expect(n).not.toBeUndefined();
+  expect(n).not.toBeTruthy();
+  expect(n).toBeFalsy();
+});
+
+test("zero", () => {
+  const z = 0;
+  expect(z).not.toBeNull();
+  expect(z).toBeDefined();
+  expect(z).not.toBeUndefined();
+  expect(z).not.toBeTruthy();
+  expect(z).toBeFalsy();
 });
