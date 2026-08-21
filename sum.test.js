c43f97ee28591fe20328b00d9d54dff1788d2caa
pbain63 <prodipbain63@gmail.com>
2026-08-21 20:40:01 +0600

    Add test for practice Arrays and iterables test in sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -4,10 +4,14 @@
 //   expect(sum(1, 2)).toBe(3);
 // });
 
-test("there is no I in team", () => {
-  expect("team").not.toMatch(/I/);
-});
-
-test('but there is a "stop" in Christoph', () => {
-  expect("Christoph").toMatch(/stop/);
+const shoppingList = [
+  "diapers",
+  "kleenex",
+  "trash bags",
+  "paper towels",
+  "milk",
+];
+test("the shopping list has milk on it", () => {
+  expect(shoppingList).toContain("milk");
+  expect(new Set(shoppingList)).toContain("milk");
 });
