48e837825920e6c49a6002962d43187b44fea2a4
pbain63 <prodipbain63@gmail.com>
2026-08-21 21:01:56 +0600

    Add test for practice Exceptions in sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -4,14 +4,17 @@
 //   expect(sum(1, 2)).toBe(3);
 // });
 
-const shoppingList = [
-  "diapers",
-  "kleenex",
-  "trash bags",
-  "paper towels",
-  "milk",
-];
-test("the shopping list has milk on it", () => {
-  expect(shoppingList).toContain("milk");
-  expect(new Set(shoppingList)).toContain("milk");
+function compileAndroidCode() {
+  throw new Error("you are using the wrong JDK!");
+}
+
+test("compiling android goes as expected", () => {
+  expect(() => compileAndroidCode()).toThrow();
+  expect(() => compileAndroidCode()).toThrow(Error);
+
+  expect(() => compileAndroidCode()).toThrow("you are using the wrong JDK");
+  expect(() => compileAndroidCode()).toThrow(/JDK/);
+
+  expect(() => compileAndroidCode()).toThrow(/^you are using the wrong JDK$/);
+  expect(() => compileAndroidCode()).toThrow(/^you are using the wrong JDK!$/);
 });
