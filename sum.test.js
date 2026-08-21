ad12ee15ff78699575a05d4dfca589d5bc7d41cc
pbain63 <prodipbain63@gmail.com>
2026-08-21 15:14:50 +0600

    Add test for practice Strings test in sum.test.js

--- sum.test.js
+++ sum.test.js
@@ -4,8 +4,10 @@
 //   expect(sum(1, 2)).toBe(3);
 // });
 
-test("adding floating point numbers", () => {
-  const value = 0.1 + 0.2;
-  expect(value).toBeCloseTo(0.3);
-  expect(value).toBe(0.3);
+test("there is no I in team", () => {
+  expect("team").not.toMatch(/I/);
+});
+
+test('but there is a "stop" in Christoph', () => {
+  expect("Christoph").toMatch(/stop/);
 });
