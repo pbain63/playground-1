0b5347b38b5346b9cd6567664b31b4cd51f63d5b
pbain63 <prodipbain63@gmail.com>
2026-08-22 22:11:46 +0600

    Add test code for practice mocha test in main.js

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-let expect = require('chai').expect;
+let FlickrFetcher = require("./flickr-fetcher.js");
 
-describe('FlickrFetcher', function() {
-    it('should exist', function() {
-        var FlickrFetcher = require('./flickr-fetcher.js');
-        expect(FlickrFetcher).to.not.be.undefined;
-    });
+describe("#photoObjToURL()", function () {
+  it("should take a photo object from Flickr and return a string", function () {
+    let input = {
+      id: "24770505034",
+      owner: "97248275@N03",
+      secret: "31a9986429",
+      server: "1577",
+      farm: 2,
+      title: "20160229090898",
+      ispublic: 1,
+      isfriend: 0,
+      isfamily: 0,
+    };
+    let expected =
+      "https://farm2.staticflickr.com/1577/24770505034_31a9986429_b.jpg";
+    let actual = FlickrFetcher.photoObjToURL(input);
+    expect(actual).to.eql(expected);
+  });
 });
