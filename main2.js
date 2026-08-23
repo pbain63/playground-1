7716ef30c9f7fdf2729c60278e8261da7eefcff0
pbain63 <prodipbain63@gmail.com>
2026-08-23 22:42:01 +0600

    Add test code for practice mocha test in main2.js

--- main2.js
+++ main2.js
@@ -1,7 +1,17 @@
 let FlickrFetcher;
 
 FlickrFetcher = {
-  photoObjToUrl: function () {
-    return "https://farm2.staticflickr.com/1577/24770505034_31a9986429_b.jpg";
+  photoObjToUrl: function (photoObj) {
+    return (
+      "https://farm" +
+      photoObj.farm +
+      ".staticflickr.com/" +
+      photoObj.server +
+      "/" +
+      photoObj.id +
+      "_" +
+      photoObj.secret +
+      "_b.jpg"
+    );
   },
 };
