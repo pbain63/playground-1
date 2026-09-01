e239851fce5d704aa446112f4ec9153a968d7579
pbain63 <prodipbain63@gmail.com>
2026-09-01 09:35:41 +0600

    Add JS code to prctice Pure Functions of TDD in main.js

--- main.js
+++ main.js
@@ -1,39 +1,20 @@
-describe("#transformPhotoObj()", function () {
-  it('should take a photo object and return an object with just title and <abbr title="uniform resource locator">URL</abbr>', function () {
-    var input = {
-        id: "25373736106",
-        owner: "99117316@N03",
-        secret: "146731fcb7",
-        server: "1669",
-        farm: 2,
-        title: "Dog goes to desperate measure to avoid walking on a leash",
-        ispublic: 1,
-        isfriend: 0,
-        isfamily: 0,
-      },
-      expected = {
-        title: "Dog goes to desperate measure to avoid walking on a leash",
-        url: "https://farm2.staticflickr.com/1669/25373736106_146731fcb7_b.jpg",
-      },
-      actual = FlickrFetcher.transformPhotoObj(input);
-    expect(actual).to.eql(expected);
+function evaluateGuess(magicNumber, guess) {
+  if (guess > magicNumber) {
+    return "Your guess is too big";
+  } else if (guess < magicNumber) {
+    return "Yor guess is too small";
+  } else if (guess == magicNumber) {
+    return "You did it";
+  } else {
+    return "INVALID INPUT";
+  }
+}
 
-    input = {
-      id: "24765033584",
-      owner: "27294864@N02",
-      secret: "3c190c104e",
-      server: "1514",
-      farm: 2,
-      title: "the other cate",
-      ispublic: 1,
-      isfriend: 0,
-      isfamily: 0,
-    };
-    expected = {
-      title: "the other cate",
-      url: "https://farm2.staticflickr.com/1514/24765033584_3c190c104e_b.jpg",
-    };
-    actual = FlickrFetcher.transformPhotoObj(input);
-    expect(actual).to.eql(expected);
-  });
-});
+function guessingGame() {
+  const magicNumber = 22;
+  const guess = prompt("Guess a number between 1 and 100!");
+  const message = evaluateGuess(magicNumber, guess);
+  alert(message);
+}
+
+guessingGame();
