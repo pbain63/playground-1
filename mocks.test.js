17a88592250252e7f3263e7949265854f77994e4
pbain63 <prodipbain63@gmail.com>
2026-09-03 09:58:48 +0600

    Add practice test case for Setup and Teardown in mocks.test.js

--- mocks.test.js
+++ mocks.test.js
@@ -1,58 +1,15 @@
-const forEach = (items, callback) => {
-  for (let i = 0; i < items.length; i++) {
-    callback(items[i]);
-  }
-};
-
-it("mock callback", () => {
-  const mockCalledback = jest.fn((x) => 42 + x);
-
-  forEach([0, 1], mockCalledback);
-
-  expect(mockCalledback.mock.calls.length).toBe(2);
-
-  expect(mockCalledback.mock.calls[0][0]).toBe(0);
-
-  expect(mockCalledback.mock.calls[1][0]).toBe(1);
-
-  expect(mockCalledback.mock.results[0].value).toBe(42);
-
-  expect(mockCalledback.mock.results[1].value).toBe(43);
+beforeAll(() => {
+  return initializeCityDatabase();
 });
 
-it("mock return", () => {
-  const mock = jest.fn();
-
-  mock
-    .mockReturnValueOnce(true)
-    .mockReturnValueOnce(false)
-    .mockReturnValueOnce("Hello");
-
-  const results = mock();
-  const results2 = mock();
-  const results3 = mock();
-
-  expect(results).toBe(true);
-  expect(results2).toBe(false);
-  expect(results3).toBe("Hello");
+afterAll(() => {
+  return clearCityDatabase();
 });
 
-//
-// const axios = require("axios");
+test("city database has Vienna", () => {
+  expect(isCity("Vienna")).toBeTruthy();
+});
 
-// const fetchData = async (id) => {
-//   const results = await axios.get(
-//     `https://jsonplaceholder.typicode.com/todos/${id}`
-//   );
-//   console.log(results);
-//   return results.data;
-// };
-
-// it("mock axios", async () => {
-//   jest.spyOn(axios, "get").mockReturnValueOnce({
-//     data: { id: 1, todo: "Get 1M Subs" },
-//   });
-//   const results = await fetchData(1);
-
-//   expect(results.todo).toBe("Get 1M Subs");
-// });
+test("city database has San Juan", () => {
+  expect(isCity("San Juan")).toBeTruthy();
+});
