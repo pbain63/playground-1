ac4888a5a690103200d1a0618902d6f37bf841eb
pbain63 <prodipbain63@gmail.com>
2026-09-03 11:06:43 +0600

    Add practice test case for Order of Execution in mocks.test.js

--- mocks.test.js
+++ mocks.test.js
@@ -1,28 +1,30 @@
-beforeAll(() => console.log('1 - beforeAll'));//
-afterAll(() => console.log('1 - afterAll'));//
-beforeEach(() => console.log('1 - beforeEach'));//
-afterEach(() => console.log('1 - afterEach'));//
+describe('describe outer', () => {
+  console.log('describe outer-a');
 
-test('', () => console.log('1 - test'));//
+  describe('describe inner 1', () => {
+    console.log('describe inner 1');
 
-describe('Scoped / Nested block', () => {
-  beforeAll(() => console.log('2 - beforeAll'));//
-  afterAll(() => console.log('2 - afterAll'));//
-  beforeEach(() => console.log('2 - beforeEach'));//
-  afterEach(() => console.log('2 - afterEach'));//
+    test('test 1', () => console.log('test 1'));
+  });
 
-  test('', () => console.log('2 - test'));//
+  console.log('describe outer-b');
+
+  test('test 2', () => console.log('test 2'));
+
+  describe('describe inner 2', () => {
+    console.log('describe inner 2');
+
+    test('test 3', () => console.log('test 3'));
+  });
+
+  console.log('describe outer-c');
 });
 
-// 1 - beforeAll-
-// 1 - beforeEach-
-// 1 - test-
-// 1 - afterEach-
-// 2 - beforeAll
-// 1 - beforeEach
-// 2 - beforeEach
-// 2 - test
-// 2 - afterEach
-// 1 - afterEach
-// 2 - afterAll
-// 1 - afterAll
+// describe outer-a
+// describe inner 1
+// describe outer-b
+// describe inner 2
+// describe outer-c
+// test 1
+// test 2
+// test 3
