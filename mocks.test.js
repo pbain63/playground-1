b405f28ff5af41eb5a161c6150cc8064651f6328
pbain63 <prodipbain63@gmail.com>
2026-09-04 14:19:03 +0600

    Add practice test case for Order of Execution in mocks.test.js

--- mocks.test.js
+++ mocks.test.js
@@ -1,30 +1,28 @@
-describe('describe outer', () => {
-  console.log('describe outer-a');
+beforeEach(() => console.log('connection setup'));
+beforeEach(() => console.log('database setup'));
 
-  describe('describe inner 1', () => {
-    console.log('describe inner 1');
+afterEach(() => console.log('database teardown'));
+afterEach(() => console.log('connection teardown'));
 
-    test('test 1', () => console.log('test 1'));
-  });
+test('test 1', () => console.log('test 1'));
 
-  console.log('describe outer-b');
+describe('extra', () => {
+  beforeEach(() => console.log('extra database setup'));
+  afterEach(() => console.log('extra database teardown'));
 
   test('test 2', () => console.log('test 2'));
-
-  describe('describe inner 2', () => {
-    console.log('describe inner 2');
-
-    test('test 3', () => console.log('test 3'));
-  });
-
-  console.log('describe outer-c');
 });
 
-// describe outer-a
-// describe inner 1
-// describe outer-b
-// describe inner 2
-// describe outer-c
+// connection setup
+// database setup
 // test 1
+// database teardown
+// connection teardown
+
+// connection setup
+// database setup
+// extra database setup
 // test 2
-// test 3
+// extra database teardown
+// database teardown
+// connection teardown
