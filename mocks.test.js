6f8463eabca0989bff71cb092fa3a66237e0b4ac
pbain63 <prodipbain63@gmail.com>
2026-09-04 14:23:45 +0600

    Add practice test case for test.inly in mocks.test.js

--- mocks.test.js
+++ mocks.test.js
@@ -1,28 +1,7 @@
-beforeEach(() => console.log('connection setup'));
-beforeEach(() => console.log('database setup'));
-
-afterEach(() => console.log('database teardown'));
-afterEach(() => console.log('connection teardown'));
-
-test('test 1', () => console.log('test 1'));
-
-describe('extra', () => {
-  beforeEach(() => console.log('extra database setup'));
-  afterEach(() => console.log('extra database teardown'));
-
-  test('test 2', () => console.log('test 2'));
+test.only("this will be the only test that runs", () => {
+  expect(true).toBe(false);
 });
 
-// connection setup
-// database setup
-// test 1
-// database teardown
-// connection teardown
-
-// connection setup
-// database setup
-// extra database setup
-// test 2
-// extra database teardown
-// database teardown
-// connection teardown
+test("this test will not run", () => {
+  expect("A").toBe("A");
+});
