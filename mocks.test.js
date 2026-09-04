6ff883354e3e5fd1cdcb9e500151fe0e26c9a40e
pbain63 <prodipbain63@gmail.com>
2026-09-04 20:18:56 +0600

    Add test case for practice .mock property in mocks.test.js

--- mocks.test.js
+++ mocks.test.js
@@ -1,15 +1,15 @@
-import { forEach } from "./mocks";
+expect(someMockFunction.mock.calls).toHaveLength(1);
 
-const mockCallback = jest.fn((x) => 42 + x);
+expect(someMockFunction.mock.calls[0][0]).toBe("first arg");
 
-test("forEach mock function", () => {
-  forEach([0, 1], mockCallback);
+expect(someMockFunction.mock.calls[0][1]).toBe("second arg");
 
-  expect(mockCallback.mock.calls).toHaveLength(2);
+expect(someMockFunction.mock.results[0].value).toBe("return value");
 
-  expect(mockCallback.mock.calls[0][0]).toBe(0);
+expect(someMockFunction.mock.contexts[0]).toBe(element);
 
-  expect(mockCallback.mock.calls[1][0].toBe(1));
+expect(someMockFunction.mock.instances.length).toBe(2);
 
-  expect(mockCallback.mock.results[0].value).toBe(42);
-});
+expect(someMockFunction.mock.instances[0].name).toBe("test");
+
+expect(someMockFunction.mock.lastCall[0]).toBe("test");
