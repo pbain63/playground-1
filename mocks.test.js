1f0431832902d021f3237c9437f151253fd758a7
pbain63 <prodipbain63@gmail.com>
2026-09-04 19:49:08 +0600

    Add practice test case for mock function in mocks.test.js

--- mocks.test.js
+++ mocks.test.js
@@ -1,7 +1,15 @@
-test.only("this will be the only test that runs", () => {
-  expect(true).toBe(false);
-});
+import { forEach } from "./mocks";
 
-test("this test will not run", () => {
-  expect("A").toBe("A");
+const mockCallback = jest.fn((x) => 42 + x);
+
+test("forEach mock function", () => {
+  forEach([0, 1], mockCallback);
+
+  expect(mockCallback.mock.calls).toHaveLength(2);
+
+  expect(mockCallback.mock.calls[0][0]).toBe(0);
+
+  expect(mockCallback.mock.calls[1][0].toBe(1));
+
+  expect(mockCallback.mock.results[0].value).toBe(42);
 });
