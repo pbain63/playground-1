f162a118dec1592ba7b61948c378c92a6ad0eeb6
pbain63 <prodipbain63@gmail.com>
2026-09-09 22:43:57 +0600

    Add code for pratice linked list in main.js

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
-let list = {
-  value: 1,
-  next: {
-    value: 2,
-    next: {
-      value: 3,
-      next: {
-        value: 4,
-        next: null,
-      },
-    },
-  },
-};
+let list = { value: 1 };
+list.next = { value: 2 };
+list.next.next = { value: 3 };
+list.next.next.next = { value: 4 };
+list.next.next.next.next = null;
