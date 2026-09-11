02bbfaf1aaac5cec257d26630bb27aa2abed4198
pbain63 <prodipbain63@gmail.com>
2026-09-11 10:46:45 +0600

    Add code for pratice linked list in main.js

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
-function fib(n) {
-  let a = 1;
-  let b = 1;
-  for (let i = 3; i <= n; i++) {
-    c = a + b;
-    a = b;
-    b = c;
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null,
+      },
+    },
+  },
+};
+
+function printList(list) {
+  let tmp = list;
+
+  while (tmp) {
+    alert(tmp.value);
+    tmp = tmp.next;
   }
-  return b;
 }
 
-alert(fib(7));
-// console.log(fib(7));
+printList(list);
