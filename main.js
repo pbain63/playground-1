5dd52247ca4306ab9f98d9a0e02b4b77972d3b8a
pbain63 <prodipbain63@gmail.com>
2026-09-11 09:02:57 +0600

    Add code for pratice fibonacci number through recursion in main.js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-function factorial(n) {
-  if ((n === 0) || (n === 1)) {
+function fib(n) {
+  if (n == 1 || n == 2) {
     return 1;
   } else {
-    return n * factorial(n - 1);
+    return fib(n - 1) + fib(n - 2);
   }
 }
 
-alert(factorial(5));
+alert(fib(7));
+// console.log(fib(7));
