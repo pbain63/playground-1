54f1fc50083ef39a84124f82a9ec24201f9db704
pbain63 <prodipbain63@gmail.com>
2026-09-11 09:20:39 +0600

    Add code for pratice fibonacci number through dynamic programming bottom-up in main.js

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 function fib(n) {
-  if (n == 1 || n == 2) {
-    return 1;
-  } else {
-    return fib(n - 1) + fib(n - 2);
+  let a = 1;
+  let b = 1;
+  for (let i = 3; i <= n; i++) {
+    c = a + b;
+    a = b;
+    b = c;
   }
+  return b;
 }
 
 alert(fib(7));
