b837cc7e3389b4f87b282160fd80c2b53bd95ac4
pbain63 <prodipbain63@gmail.com>
2026-09-12 19:07:54 +0600

    Add code for pratice loop in main.js

--- main.js
+++ main.js
@@ -1,29 +1,8 @@
-let list = {
-  value: 1,
-  next: {
-    value: 2,
-    next: {
-      value: 3,
-      next: {
-        value: 4,
-        next: null,
-      },
-    },
-  },
-};
-
-function printReverseList(list) {
-  let arr = [];
-  let tmp = list;
-
-  while (tmp) {
-    arr.push(tmp.value);
-    tmp = tmp.next;
+function countDown(n) {
+  for (let i = n; i > 0; i--) {
+    console.log(i);
   }
-
-  for (let i = arr.length - 1; i >= 0; i--) {
-    alert(arr[i]);
-  }
+  console.log("Horray");
 }
 
-printReverseList(list);
+countDown(3);
