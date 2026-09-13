9e9cc951dd34f0ca65aced2ded91fbe3cd5dc12a
pbain63 <prodipbain63@gmail.com>
2026-09-13 21:58:51 +0600

    Add code for pratice recursion in main.js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-function sumRange(n) {
-  let total = 0;
-  for (let i = n; i > 0; i--) {
-    total += i;
+function sumRangeRecursive(n, total = 0) {
+  if (n <= 0) {
+    return total;
   }
-  return total;
+  return sumRangeRecursive(n - 1, total + n);
 }
 
-console.log(sumRange(3));
+console.log(sumRangeRecursive(3));
